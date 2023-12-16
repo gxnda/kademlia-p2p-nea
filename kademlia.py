a5a38281cd186d0cb65e23a0e1747e15df760d85
gxnda <116901054+gxnda@users.noreply.github.com>
2023-12-16 02:29:13 +0000

    feat: added MyLock() object which can be used in "with" calls.

--- kademlia.py
+++ kademlia.py
@@ -34,6 +34,45 @@ class SendingQueryToSelfError(Exception):
     pass
 
 
+class MyLock():
+    """
+    Lock object that can be used in "with" statements.
+
+    Example usage:
+
+        lock = threading.Lock()
+
+        with MyLock(lock):
+            do_stuff()
+
+        do_more_stuff()
+
+    Based from the following code:
+
+    https://www.bogotobogo.com/python/Multithread/python_multithreading_Synchronization_Lock_Objects_Acquire_Release.php
+
+    https://www.geeksforgeeks.org/with-statement-in-python/
+
+    """
+    def __init__(self, lock: Lock) -> None:
+        """
+        Creates lock object to be used in __enter__ and __exit__.
+        """
+        self.lock = lock
+
+    def __enter__(self) -> None:
+        """
+        Change the state to locked and returns immediately.
+        """
+        self.lock.acquire()
+
+    def __exit__(self) -> None:
+        """
+        Changes the state to unlocked; this is called from another thread.
+        """
+        self.lock.release()
+
+
 class Constants:
 
     def __init__(self):
