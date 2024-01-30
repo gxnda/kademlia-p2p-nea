929972895a448b7b23aa72c2de1fa786ae54c29e
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2024-01-30 21:43:27 +0000

    feat: created a sensible looking process_request() file.

--- networking.py
+++ networking.py
@@ -1,7 +1,12 @@
+import threading
 from typing import TypedDict
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import pickle
 
+
+class UnknownRequestError(Exception):
+    pass
 
 
 class BaseRequest(TypedDict):
@@ -59,7 +64,7 @@ class StoreSubnetRequest(StoreRequest, ITCPSubnet, TypedDict):
 
 class CommonRequest(TypedDict):
     """
-    For passing to Node handlers with common parameters.
+    This includes all possible headers that could be passed.
     """
     protocol: object
     protocol_name: str
@@ -103,45 +108,114 @@ class StoreResponse(BaseResponse):
 
 
 class TCPServer:  # TODO: Create.
-    def __init__(self):
-        pass
+    def __init__(self, node):
+        self.route_packets = {
+            "//Ping": PingRequest,
+            "//Store": StoreRequest,
+            "//FindNode": FindNodeRequest,
+            "//FindValue": FindValueRequest
+        }
+        self.node = node
 
-    def process_request(context: BaseHTTPRequestHandler):
+
+class TCPSubnetServer(HTTPServer):
+    def __init__(self, server_address=tuple[str, int]):
+        super().__init__(
+            server_address=server_address,
+            RequestHandlerClass=BaseHTTPRequestHandler
+        )
+
+
+        # TODO: Should these be double slashed?
+        self.route_packets: dict[str: type] = {
+            "//ping": PingRequest,  # "ping" should refer to type PingRequest
+            "//store": StoreRequest,  # "store" should refer to type StoreRequest
+            "//find_node": FindNodeRequest,  # "find_node" should refer to type FindNodeRequest
+            "//find_value": FindValueRequest  # "find_value" should refer to type FindValueRequest
+        }
+
+        self.subnets: dict = {}
+
+    def register_protocol(self, subnet: int, node):
+        self.subnets[subnet] = node
+
+    def process_request(self, context: BaseHTTPRequestHandler):
         """
         I don't know much about HTTP Servers.
         "The server is a straightforward HttpListener implemented as a C# HttpListenerContext
         object, but note how the subnet ID is used to route the request to the specific node
         associated with the subnet." - Marc Clifton
 
-        :param context:
-        :return:
+        I am planning to take in a BaseHTTPRequestHandler object and read the body.
+        The body should be a JSON containing key value pairs of the following values:
+
+            protocol: object
+            protocol_name: str
+            random_id: int
+            sender: int
+            key: int
+            value: int
+            is_cached: bool
+            expiration_time_sec: int
+            subnet: int
         """
-        data: str = context.request
+        context.handle_one_request()
+
+        # TODO: Add Encryption !!!!!!!
+
+        encoded_request: bytes = context.rfile.read()
+        decoded_request: str = encoded_request.decode("utf-8")
+        request = decoded_request
+
+        key_value_pickled_pairs: dict = json.loads(request)
+        request_dict: dict = {}
+        for key in key_value_pickled_pairs:
+            # All objects are sent using HTTP as pickle objects to keep protocol objects intact.
+            request_dict[key] = pickle.loads(key_value_pickled_pairs[key])
+
         print(context.request, context.command)
         if context.command == "POST":
-            request_type: type
             path: str = context.path
             # Remove "//"
-            # Prefix our call with "server" so that the method name is unambiguous.
+            # Prefix our call with "Server" so that the method name is unambiguous.
             method_name: str = "Server" + path[2:]  # path.substring(2)
-            if route_packets.try_get_value(path, request_type):
-                commonrequest: CommonRequest = json.load(data)
-                subnet: int = json.load(data, request_type)["subnet"]
 
-                node: INode
-                if subnets.try_get_value(subnet, node):
-                    # await Task.run()
-                    Task.run(lambda: CommonRequestHandler(
-                        method_name,
-                        commonrequest,
-                        node,
-                        context
-                        )
+            # What type is the request?
+            try:
+                # path is something like //ping or //find_node
+                request_type: type | None = self.route_packets[path]
+            except KeyError:
+                request_type: type | None = None
+
+            # if we know what the request wants (if it's a ping/find_node RPC etc.)
+            if request_type:
+                subnet: int = request_dict["subnet"]
+                common_request: CommonRequest = CommonRequest(
+                    protocol=request_dict.get("protocol"),
+                    protocol_name=request_dict.get("protocol_name"),
+                    random_id=request_dict.get("random_id"),
+                    sender=request_dict.get("sender"),
+                    key=request_dict.get("key"),
+                    value=request_dict.get("value"),
+                    is_cached=request_dict.get("is_cached"),
+                    expiration_time_sec=request_dict.get("expiration_time_sec")
+                )
+
+                # If we know the node on the subnet, this should always happen right?
+                # Because this is for testing on the same PC.
+                node = self.subnets.get(subnet)
+                if node:
+                    # TODO: Make Asynchronous
+                    new_thread = threading.Thread(
+                        target=CommonRequestHandler,  # TODO: This function does not exist.
+                        args=(method_name, common_request, node, context)
                     )
+                    new_thread.start()
+
                 else:
                     send_error_response(
                         context,
                         ErrorResponse("Subnet node not found.")
                     )
 
-                context.response.close()
+                # context.close_connection = True
