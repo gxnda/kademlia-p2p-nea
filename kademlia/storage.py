c7678b1d3c99fa0f5eb2d3a0172746bb98b4bb45
Gabriel Lancaster-West <gabriellamcasterwest@gmail.com>
2024-02-06 13:10:59 +0000

    feat: made VirtualStorage

--- kademlia/storage.py
+++ kademlia/storage.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from kademlia.dictionaries import StoreValue
 from kademlia.id import ID
 from kademlia.interfaces import IStorage
 
@@ -10,7 +11,7 @@ class VirtualStorage(IStorage):
     """
 
     def __init__(self):
-        self._store: dict[int, str] = {}
+        self._store: dict[int, StoreValue] = {}
 
     def contains(self, key: ID) -> bool:
         """
@@ -18,7 +19,7 @@ class VirtualStorage(IStorage):
         """
         return key.value in list(self._store.keys())
 
-    def get(self, key: ID | int) -> str:
+    def get(self, key: ID | int) -> StoreValue:
         """
         Returns stored value, associated with given key value.
         :param key: Type ID or Integer, key value to be searched.
@@ -32,23 +33,30 @@ class VirtualStorage(IStorage):
             raise TypeError("'get()' parameter 'key' must be type ID or int.")
 
     def get_timestamp(self, key: int) -> datetime:
-        pass
+        return self._store[key]["republish_timestamp"]
 
     def set(self, key: ID, value: str, expiration_time_sec: int = 0) -> None:
-        self._store[key.value] = value
-        # TODO: Add expiration time to this.
+        self._store[key.value] = StoreValue(value=value, expiration_time=expiration_time_sec)
+        self.touch(key.value)
 
     def get_expiration_time_sec(self, key: int) -> int:
-        pass
+        return self._store[key]["expiration_time"]
 
     def remove(self, key: int) -> None:
-        pass
+        if key in self._store:
+            self._store.pop(key, None)
 
     def get_keys(self) -> list[int]:
         return list(self._store.keys())
 
     def touch(self, key: int) -> None:
-        pass
+        self._store[key]["republish_timestamp"] = datetime.now()
 
-    def try_get_value(self, key: ID) -> tuple[bool, int | str]:
-        pass
+    def try_get_value(self, key: ID) -> tuple[bool, int | str | None]:
+        val = None
+        ret = False
+        if key.value in self._store:
+            val = self._store[key.value]
+            ret = True
+
+        return ret, val
